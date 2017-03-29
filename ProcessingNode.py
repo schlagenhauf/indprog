02f84713a5ffbb797d31663ff797f1254f0b0ac3
Jonas <jonas.schlagenhauf@gmx.de>
2017-03-29 18:52:25 +0200

    Add processing node class

---
+++ ProcessingNode.py
@@ -0,0 +1,14 @@
+
+class ProcessingNode:
+    def __init__(self):
+        pass
+        self.inputPorts = []
+        self.outputPorts = []
+        self.process = None
+
+    def process(self):
+        pass
+
+
+    def connectTo(self, myPort, otherPort):
+        pass
