977fc506643b3143053173399f9cb729ce5421dc
Jonas <jonas.schlagenhauf@gmx.de>
2017-04-02 14:34:59 +0200

    Add fd pipes to communicate between ports

--- Processing.py
+++ Processing.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+
 
 class ProcessingGraph:
     def __init__(self):
@@ -10,18 +12,18 @@ class ProcessingGraph:
         self.nodes.append(node)
 
 
-    def connectPorts(self, portA, portB):
-        if portA.direction == portB.direction:
+    def connectPorts(self, portFrom, portTo):
+        if portFrom.direction == portTo.direction:
             return False
         else:
-            portA.connectedTo = portB
-            portB.connectedTo = portA
+            portFrom.connectedTo = portTo
+            portTo.connectedTo = portFrom
+            portTo.data = portFrom.data
             return True
 
 
     def process(self, node):
         nodeList = self.getNodesRecurs(node)
-        print(nodeList)
         for n in nodeList:
             n.process()
 
@@ -42,50 +44,82 @@ class ProcessingGraph:
         return [x for x in seq if not (x in seen or seen.add(x))]
 
 
-
+##
+# @brief A node bundles a process with input and output ports.
 class ProcessingNode:
-    def __init__(self, name):
+    def __init__(self, name, inPorts = [], outPorts = []):
         self.name = name
-        self.inputPorts = [Port(self, "in")]
-        self.outputPorts = [Port(self, "out")]
-        self.proc = Process()
+        self.inputPorts = [Port(self, ip) for ip in inPorts]
+        self.outputPorts = [Port(self, op) for op in outPorts]
+        self.proc = Process(name)
 
     def process(self):
         print('Node ' + self.name + ':')
-        self.proc.setInData([ip.data for ip in self.inputPorts])
-        self.proc.run()
-        outdata = self.proc.getOutData()
+        inFds = [ip.data[0] for ip in self.inputPorts]
+        outFds = [op.data[1] for op in self.outputPorts]
+        self.proc.run(inFds, outFds)
 
 
+##
+# @brief A port represents one of possibly many inputs or outputs of a node.
+# If an output port is created, a filesystem pipe is created along with it
 class Port:
     def __init__(self, node, direction):
         self.node = node
         self.direction = direction
         self.connectedTo = None
-        self.data = ""
+
+        if direction == "in":
+            self.data = None
+        else:
+            self.data = os.pipe()
 
 
+##
+# @brief Wrapper for the process that a node represents. Can wrap a variety of actions.
 class Process:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
 
-    def setInData(self, indata):
-        pass
+    def run(self, inFds, outFds):
+        str = ""
+        for i in inFds:
+            iop = open(i, 'r')
+            if iop:
+                str += iop.readline()
+                iop.close()
 
-    def getOutData(self):
-        pass
+        str += self.name
 
-    def run(self):
-        print('\tprocessing...')
+        print(str)
+
+        for o in outFds:
+            oop = open(o, 'w')
+            if oop:
+                oop.write(str)
+                oop.close()
+
+        #print(inFds)
+        #print(outFds)
+        #print('\tDummy Process...')
+
+
+class ArithmeticProcess(Process):
+    def __init__(self, procType):
+        self.procType = procType
+
+    def run(self, inFds, outFds):
+        if self.procType == "add":
+           outdata.append([inFd[0] + inFd[1]])
+
 
 
 if __name__ == '__main__':
     graph = ProcessingGraph()
-    node1 = ProcessingNode("node1")
-    node2 = ProcessingNode("node2")
-    node2.inputPorts.append(Port(node2, "in"));
-    node3 = ProcessingNode("node3")
-    node4 = ProcessingNode("node4")
+    node1 = ProcessingNode("node1", ["in"], ["out"])
+    node2 = ProcessingNode("node2", ["in1", "in2"], [])
+    node3 = ProcessingNode("node3", [], ["out"])
+    node4 = ProcessingNode("node4", [], ["out"])
     graph.addNode(node1)
     graph.addNode(node2)
     graph.addNode(node3)
